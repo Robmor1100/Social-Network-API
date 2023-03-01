3a9786fb4834d8ba58d385b6c11c11bfb81b5899
Robert Morales <morales.jrobert45@yahoo.com>
2023-03-01 00:31:40 -0800

    fixed a route

--- models/Thought.js
+++ models/Thought.js
@@ -41,3 +41,4 @@ const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
 
+
--- server.js
+++ server.js
@@ -15,3 +15,4 @@ db.once('open', () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
+
